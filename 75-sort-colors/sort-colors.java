4b523721475df7c6e358cabbb74151cff18e8966
sidharth ram <acnellithave@gmail.com>
2024-04-01 09:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (34.84%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,30 +1,5 @@
 class Solution {
     public void sortColors(int[] nums) {
-        quickSort(nums,0,nums.length-1);
-    }
-    public void quickSort(int[] nums,int low,int high){
-        if(low>=high){
-            return ;
-        }
-        int right=high;
-        int left=low;
-        int pivot=nums[high];
-        while(left<right){
-            while(left<right&&nums[left]<=pivot){
-                left++;
-            }
-            while(left<right&&nums[right]>=pivot){
-                right--;
-            }
-            swap(nums,left,right);
-        }
-        swap(nums,left,high);
-        quickSort(nums,low,left-1);
-        quickSort(nums,left+1,high);
-    }
-    public void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+       Arrays.sort(nums);
     }
 }
